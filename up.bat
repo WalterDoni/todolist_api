6d96c9207e404ef078321ebbe646fc81aaecc402
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-02-06 13:34:40 +0100

    first_working_version

--- up.bat
+++ up.bat
@@ -1,4 +1,4 @@
 git add .
 git commit -m "%*"
 git push
-chrome "https://www.pythonanywhere.com/user/a/consosdasdasdasdles/asdasd/"
+chrome 'https://www.pythonanywhere.com/user/Walter91/consoles/32263768/'
